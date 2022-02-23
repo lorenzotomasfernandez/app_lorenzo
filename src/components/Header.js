38e8b773356feb16b85bc9b57bca042718b56aee
Lorenzo Fernandez <lorenzotomasf@gmail.com>
2022-02-23 14:30:55 -0300

    Desafio 2

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import { NavBar } from "./NavBar";
+import './Header.css';
+
+function Header() {
+    return (
+        
+            <header className="encabezado">
+               <NavBar/>                
+            </header>
+        )
+    }
+
+export default Header;
